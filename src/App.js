77f7908ee3bc3460a93b5411b38eb37f1bcfc660
onezach <87786580+onezach@users.noreply.github.com>
2023-06-02 17:24:13 -0500

    demo

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import "./App.scss";
 import DraftBoard from "./components/DraftBoard";
 
 function App() {
-  const numRounds = 2;
+  const numRounds = 15;
   // const numTeams = 12;
 
-  // const teams = [
-  //   "Team 1",
-  //   "Team 2",
-  //   "Team 3",
-  //   "Team 4",
-  //   "Team 5",
-  //   "Team 6",
-  //   "Team 7",
-  //   "Team 8",
-  //   "Team 9",
-  //   "Team 10",
-  //   "Team 11",
-  //   "Team 12",
-  // ];
+  const teams = [
+    "Team 1",
+    "Team 2",
+    "Team 3",
+    "Team 4",
+    "Team 5",
+    "Team 6",
+    "Team 7",
+    "Team 8",
+    "Team 9",
+    "Team 10",
+    "Team 11",
+    "Team 12",
+  ];
 
-  const teamsSmall = ["Team 1", "Team 2"];
+  // const teamsSmall = ["Team 1", "Team 2"];
 
   const timePerPick = 15; // seconds
 
@@ -29,7 +29,7 @@ function App() {
     <div className="App-container">
       <DraftBoard
         numRounds={numRounds}
-        teams={teamsSmall}
+        teams={teams}
         timePerPick={timePerPick}
       />
     </div>
